3b88ed4eb926ff509496326e0ff72686b5f850cd
Home <picccc89@gmail.com>
2023-01-06 00:39:39 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    //if (n > 0) {
+    //    console.log(n)
+    let number = Math.abs(n)
+    return (number + '').split('').reverse().join('');
+    //} else {
+    //    console.log(n)
+    //    return `${(n + '').replace('-','').split('').reverse().join('')}`;
+    //}
 }
