bc7d43cd90a2c7fbcd575a97fcda963dca8fa973
troay89 <picccc89@gmail.com>
2024-07-31 19:56:13 +0300

    feat: improve

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 module.exports = function reverse(n) {
-    //if (n > 0) {
-    //    console.log(n)
-    let number = Math.abs(n)
-    return (number + '').split('').reverse().join('');
-    //} else {
-    //    console.log(n)
-    //    return `${(n + '').replace('-','').split('').reverse().join('')}`;
-    //}
+    return (Math.abs(n) + '').split('').reverse().join('');
 }
